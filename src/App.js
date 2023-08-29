755f2bf9b1eb5e248cd4b3122fa78110644fb496
Atomreirun <rei_0615@yahoo.com>
2023-08-29 19:34:20 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was created by Arlene Runyon and is{" "}
-        <a href="" target="_blank">
-          open-sourced on Github.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was created by Arlene Runyon and is{" "}
+          <a
+            href="https://github.com/Atomreirun/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on Github.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

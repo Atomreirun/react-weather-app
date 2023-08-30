d56eda5340ae695dff0c34466fbfda8ddff00a81
Atomreirun <rei_0615@yahoo.com>
2023-08-30 19:40:15 -0400

    added basic styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
+import Weather from "./Weather";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was created by Arlene Runyon and is{" "}
           <a
             href="https://github.com/Atomreirun/react-weather-app"
             target="_blank"
+            rel="noreferrer"
           >
             open-sourced on Github.
           </a>

edd900ae7dbe87acb8937210065975ac2d554378
Atomreirun <rei_0615@yahoo.com>
2023-09-01 09:01:25 -0400

    added date to my project

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather default="New York" />
         <footer>
           This project was created by Arlene Runyon and is{" "}
           <a
